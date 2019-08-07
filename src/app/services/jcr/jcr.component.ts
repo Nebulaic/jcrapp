5b462360c065206d01083e322697a28b11617867
Ben Roberts <broberts98@hotmail.co.uk>
2019-08-07 12:11:17 +0100

    Working Angular baseline

---
+++ src/app/services/jcr/jcr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jcr',
+  templateUrl: './jcr.component.html',
+  styleUrls: ['./jcr.component.scss']
+})
+export class JcrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

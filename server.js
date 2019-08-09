83e3ef9f923aef3fe3e4ec01b3b001b9149a7b2f
Ben Roberts <broberts98@hotmail.co.uk>
2019-08-09 14:27:10 +0100

    Successfully established express server and api

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-// Dependencies
-const express = require('express'),
-    app = express(),
-    port = process.env.PORT || 8080;
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
 
-// Serve static page
-app.use(express.static(__dirname + '/dist/jcrapp'))
+// Get our API routes
+const api = require('./server/routes/api');
 
-// Start
-app.listen(port, () => {
-    console.log(`App listening on http://localhost:${port}`);
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist/jcrapp')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/jcrapp/index.html'));
 });
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));

9e8498687be9eb7b078e68bef351988d19060fff
Ben Roberts <broberts98@hotmail.co.uk>
2019-08-13 15:13:55 +0100

    Cleaned & commented server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require ('dotenv').config(); // Load environment variables
+ // Load environment variables
+ require ('dotenv').config();
 
 // Get dependencies
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 // Initialise app
@@ -12,23 +12,19 @@ const app = express();
 
 // API routes
 const api = require('./server/routes');
+app.use('/api', api);
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, '/dist/jcrapp')));
-
-// Set our api routes
-app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/jcrapp/index.html'));
 });
 
-
  // Get port from environment and store in Express
 const port = process.env.PORT || '3000';
 app.set('port', port);
-
 
 // Create HTTP server
 const server = http.createServer(app);
